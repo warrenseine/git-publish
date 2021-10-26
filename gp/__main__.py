71125875476d8ab9d3b5d866dd3ddcabdf7f5c77
Warren Seine <w.seine@criteo.com>
2021-10-26 21:47:40 +0200

    ✨ Parse arguments.

--- gp/__main__.py
+++ gp/__main__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
 
     from gp.main import main
 
-    sys.exit(main())
+    sys.exit(main(sys.argv[1:]))
--- gp/main.py
+++ gp/main.py
@@ -1,2 +1,14 @@
-def main():
-    print("hello world")
+import argparse
+
+version = "0.0.1"
+
+
+def main(argv: list[str]):
+    parser = argparse.ArgumentParser(
+        prog="git-publish", description="Publish atomic Git commits."
+    )
+    parser.add_argument(
+        "-v", "--version", action="version", version=f"%(prog)s {version}"
+    )
+
+    args = parser.parse_args(argv)
