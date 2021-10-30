1a0ae1e3a1808a6798d0a560d691f685122bd73f
Warren Seine <w.seine@criteo.com>
2021-10-30 22:42:16 +0200

    ✨ Add GitHub support.
    Change-Id: w.seine-de3a

--- gp/gitproject.py
+++ gp/gitproject.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 from os import getenv
 from typing import Optional
 from git import Head, Remote
+from github import Github
+from github.PullRequest import PullRequest
 from gitlab.client import Gitlab
 from gitlab.v4.objects import ProjectManager
 from gitlab.v4.objects.merge_requests import (
@@ -27,7 +29,46 @@ def build_git_project(remote: Remote) -> GitProject:
     if git_url.platform == "gitlab":
         return GitlabProject(project_namespace)
 
+    if git_url.platform == "github":
+        return GithubProject(project_namespace)
+
     raise NotImplementedError(f"Unknown Git platform {git_url.platform}.")
+
+
+class GithubProject(GitProject):
+    def __init__(self, project_namespace: str):
+        github_token = getenv("GITHUB_TOKEN")
+        if not github_token:
+            raise EnvironmentError(f"Empty environment variable GITHUB_TOKEN.")
+        self.github = Github(github_token)
+        self.project = self.github.get_repo(project_namespace)
+        self.pull_requests = self.project.get_pulls()
+
+    def create_or_update_change(
+        self, change_id: str, source_branch: Head, target_branch: Head, title: str
+    ):
+        pull_request = self.__find_pull_request(change_id)
+
+        if pull_request:
+            self.__update_pull_request(pull_request, target_branch, title)
+        else:
+            self.__create_pull_request(source_branch, target_branch, title)
+
+    def __find_pull_request(self, source_branch: str) -> Optional[PullRequest]:
+        for pull_request in self.pull_requests:
+            if pull_request.head.ref == source_branch:
+                return pull_request
+        return None
+
+    def __update_pull_request(
+        self, pull_request: PullRequest, target_branch: Head, title: str
+    ):
+        pull_request.edit(base=target_branch.name, title=title)
+
+    def __create_pull_request(
+        self, source_branch: Head, target_branch: Head, title: str
+    ):
+        self.project.create_pull(title, "", target_branch.name, source_branch.name)
 
 
 class GitlabProject(GitProject):
