42f83062524dc0708918109ceb7a202d8e104121
Warren Seine <w.seine@criteo.com>
2021-10-30 23:13:09 +0200

    ♻️️ Abstract Gitlab away.
    Change-Id: w.seine-f162

--- typings/giturlparse.pyi
+++ typings/giturlparse.pyi
@@ -1,1 +1,21 @@
-def parse(url: str) -> object: ...
+from dataclasses import dataclass
+
+
+@dataclass
+class GitUrl:
+    platform: str
+    host: str
+    resource: str
+    port: int
+    protocol: str
+    protocols: list[str]
+    user: str
+    owner: str
+    repo: str
+    name: str
+    groups: list[str]
+    path: str
+    path_raw: str
+    branch: str
+
+def parse(url: str) -> GitUrl: ...
